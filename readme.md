9631389b98e57acfa8eb4c473c623e2da022a7df
winewei <winewei@yeah.net>
2020-09-19 19:05:13 +0800

    feat: enable cycle count

--- readme.md
+++ readme.md
@@ -5,18 +5,19 @@
 git clone https://github.com/winewei/lua-waf.git
 cd lua-waf
 docker-compose up -d
-
 ```
 
 ## Set the block
 - update web site you would like to protect in [`proxy.conf`](proxy.conf)
   ```
-    proxy_pass  https://localhost;
-    proxy_set_header Host "localhost";
-
+  proxy_pass  https://localhost;
+  proxy_set_header Host "localhost";
   ```
 - update [`docker-compose`](docker-compose.yml) environment
     ```
-    BLOCK_EXPIRE: 5
-    BLOCK_LIMIT: 10
+    COUNT_CYCLE: 10
+    BASE_BAN_LIMIT: 20
+    BASE_BAN_EXPIRE: 10
+    SUPER_BAN_LIMIT: 100
+    SUPER_EXPIRE: 3600
     ```
--- waf-scripts/filter.lua
+++ waf-scripts/filter.lua
@@ -1,16 +1,11 @@
--- ngx.exit(403)
+-- access filter
 require "resty.core"
 
-local block_expire = tonumber(os.getenv("BLOCK_EXPIRE"))
-local block_limit = tonumber(os.getenv("BLOCK_LIMIT"))
-
-if block_expire == nil then
-   block_expire = 30
-end
-
-if block_limit == nil then
-   block_limit = 20
-end
+local count_cycle = tonumber(os.getenv("COUNT_CYCLE")) or 10
+local base_ban_limit = tonumber(os.getenv("BASE_BAN_LIMIT")) or 20
+local base_ban_expire = tonumber(os.getenv("BASE_BAN_EXPIRE")) or 600
+local super_ban_limit = tonumber(os.getenv("SUPER_BAN_LIMIT")) or 500
+local super_expire = tonumber(os.getenv("SUPER_EXPIRE")) or 3600
 
 -- get remote ip address
 local remote_ip = ngx.req.get_headers()["X-Real-IP"]
@@ -21,13 +16,25 @@ if remote_ip == nil then
    remote_ip = ngx.var.remote_addr
 end
 
-local dicts = ngx.shared.filter_dict
+local dict = ngx.shared.filter_dict
 local filter_key = remote_ip .. ngx.var.uri
-dicts:safe_add(filter_key, 1, block_expire)
-dicts:incr(filter_key, 1)
 
-local is_check, err = dicts:get(filter_key)
+dict:safe_add(filter_key, 1, count_cycle)
+dict:incr(filter_key, 1)
 
-if is_check > block_limit then
-    ngx.exit(403)
+local base_counts, err = dict:get(filter_key)
+
+-- filter
+if base_counts >= super_ban_limit then
+   dict:set(filter_key, base_counts, super_expire)
+   dict:incr(filter_key, 1)
+   ngx.log(ngx.ERR, "super_ban ==> ", "count: ", base_counts, " , key: ", filter_key)
+   ngx.exit(403)
+elseif base_counts >= base_ban_limit then
+   dict:set(filter_key, base_counts, base_ban_expire)
+   dict:incr(filter_key, 1)
+   ngx.log(ngx.ERR, "base_ban ==> ", "count: ", base_counts, " , key: ", filter_key)
+   ngx.exit(403)
+else
+   dict:incr(filter_key, 1)
 end
