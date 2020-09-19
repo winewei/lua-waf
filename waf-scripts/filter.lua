8f8649c2cd935cf4da87b765f4283c6cfa7c3058
winewei <winewei@yeah.net>
2020-09-19 19:06:19 +0800

    chore: update default ban limit, expire

--- waf-scripts/filter.lua
+++ waf-scripts/filter.lua
@@ -2,9 +2,9 @@
 require "resty.core"
 
 local count_cycle = tonumber(os.getenv("COUNT_CYCLE")) or 10
-local base_ban_limit = tonumber(os.getenv("BASE_BAN_LIMIT")) or 20
+local base_ban_limit = tonumber(os.getenv("BASE_BAN_LIMIT")) or 30
 local base_ban_expire = tonumber(os.getenv("BASE_BAN_EXPIRE")) or 600
-local super_ban_limit = tonumber(os.getenv("SUPER_BAN_LIMIT")) or 500
+local super_ban_limit = tonumber(os.getenv("SUPER_BAN_LIMIT")) or 100
 local super_expire = tonumber(os.getenv("SUPER_EXPIRE")) or 3600
 
 -- get remote ip address
